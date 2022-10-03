dd78747cd0b8659f2e7118ef0d93791258ba83e1
KostyaM <graffkost@mail.ru>
2022-10-03 16:20:27 +0300

    plus tabs

--- js/common.js
+++ js/common.js
@@ -4,3 +4,19 @@ new Swiper('.swiper', {
 		prevEl: '.subHeaderSlider__arrowPrev'
 	},
 });
+
+$(document).ready(function () {
+	//=== tabs
+	$('.tabs__triggers-item').click(function (e) {
+		e.preventDefault();
+		
+		$('.tabs__triggers-item').removeClass('tabs__triggers-item_active');
+		$('.tabs__content-item').removeClass('tabs__content-item_active');
+		
+		$(this).addClass('tabs__triggers-item_active');
+		$($(this).attr('href')).addClass('tabs__content-item_active');
+	});
+	
+	$('.tabs__triggers-item:first').click();
+	//===
+});
